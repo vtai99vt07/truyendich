92d201f13673aca4745fc0aa2e0a105ebe4ddcc1
Conan Vu <66713318+vtai99vt07@users.noreply.github.com>
2023-03-30 01:43:18 +0700

    Initital commit

--- app/Console/Commands/AutoLeechTrxs.php
+++ app/Console/Commands/AutoLeechTrxs.php
@@ -48,16 +48,16 @@ class AutoLeechTrxs extends Command
             do {
                 $bot = new LeechTrxs();
                 if (Carbon::now()->diffInDays(setting('leech_trxs_last_success_date', Carbon::now())) > 1) {
-                    setting()->put('leech_trxs_book_id', setting('leech_trxs_last_success_id', 1) + 1);
+                    setting()->put('leech_trxs_book_id', setting('leech_trxs_last_success_id', 5003) + 1);
                 }
-                $bookId = setting('leech_trxs_book_id', 1);
+                $bookId = setting('leech_trxs_book_id', 5003);
                 if ($bot->scrape($bookId, self::URL, '/tongren/', $admin)) {
                     $count++;
-                    setting()->put('leech_trxs_last_success_id', setting('leech_trxs_book_id', 1));
+                    setting()->put('leech_trxs_last_success_id', setting('leech_trxs_book_id', 5003));
                     setting()->put('leech_trxs_last_success_date', Carbon::now());
                 }
-                setting()->put('leech_trxs_book_id', setting('leech_trxs_book_id', 1) + 1);
-            } while ($count < 5 || setting('leech_trxs_book_id', 1) - setting('leech_trxs_last_success_id', 1) > 10000);
+                setting()->put('leech_trxs_book_id', setting('leech_trxs_book_id', 5003) + 1);
+            } while ($count < 5 || setting('leech_trxs_book_id', 5003) - setting('leech_trxs_last_success_id', 5003) > 10000);
         }
         return 0;
     }
